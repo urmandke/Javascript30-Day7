e318ca06bde61b1e7817ffa966e725d1eb2bad2a
Udayraj <udayraj.m@bizamica.com>
2021-08-10 19:16:12 +0530

    Refactor: Refactor isAdult into minimal one line code using array.some

--- script.js
+++ script.js
@@ -17,12 +17,14 @@
 
     // Some and Every Checks
     // Array.prototype.some() // is at least one person 19 or older?
-    const isAdult = people.some(function(person){
-        const currentYear = (new Date()).getFullYear();
-        if(currentYear - person.year >= 19){
-            return true;
-        }
-    });
+    // const isAdult = people.some(function(person){
+    //     const currentYear = (new Date()).getFullYear();
+    //     if(currentYear - person.year >= 19){
+    //         return true;
+    //     }
+    // });
+
+    const isAdult = people.some(person => ((new Date()).getFullYear() - person.year) >= 19);
 
     console.log({isAdult});
 
