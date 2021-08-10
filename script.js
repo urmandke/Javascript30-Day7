cfc3d2bcc1acc181df77f4f154db35d3304c9bfd
Udayraj <udayraj.m@bizamica.com>
2021-08-10 19:28:16 +0530

    Feat:Find and delete a comment with the id - array.findIndex() feature complete

--- script.js
+++ script.js
@@ -49,3 +49,9 @@
     // Array.prototype.findIndex()
     // Find the comment with this ID
     // delete the comment with the ID of 823423
+
+    const index = comments.findIndex(comment => comment.id == 823423);
+    // comment.splice (index,1);
+    const newComments = [...comments.slice(0,index), ...comments.slice(index+1)]
+
+    console.log(newComments);
